9c840c7d0e563b6301c0d5938753e23d86bd8b62
Valerio Di Zio <valerio.dizio@studio.unibo.it>
2021-03-08 22:26:18 +0100

    Aggiunta view base e main per lanciarla

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,35 +1,40 @@
 package application;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+
 
 /**
  * This class represent the Main class of the JavaFX-based application.
  */
-public final class Main extends Application {
 
-    private static final int SCENE_WIDTH = 500;
-    private static final int SCENE_HEIGHT = 300;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import model.Enemy;
+import model.Zombie;
+import view.ViewManager;
+ 
+public class Main extends Application {
+    
+    
     @Override
-    public void start(final Stage stage) throws Exception {
-        final Parent root = FXMLLoader.load(ClassLoader.getSystemResource("layouts/main.fxml"));
-        final Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
-        // Stage configuration
-        stage.setTitle("JavaFX - Complete Example");
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    /**
-     * 
-     * @param args unused
-     */
-    public static void main(final String[] args) {
-        launch();
-    }
-
+    public void start(Stage primaryStage) {
+       try {
+    	   ViewManager manager = new ViewManager();
+    	   primaryStage = manager.getMainStage();
+    	   primaryStage.show();
+       }
+       catch(Exception e){
+    	   e.printStackTrace();
+    	   
+       }
+    } 
+    public static void main(String[] args) {
+        launch(args);
+    }   
 }
+
