6665d83dca0300068e311f2c7c9ba047df56ad1b
Matteo Belletti <matteo.belletti5@studio.unibo.it>
2021-03-12 23:43:50 +0100

    eliminate weapon and fiveSeven, modify Bullet

--- src/model/Bullet.java
+++ src/model/Bullet.java
@@ -1,11 +1,45 @@
 package model;
 
-import model.Weapon.Direction;
-
 public class Bullet extends Entity implements Moveable{
-	public Bullet(Direction dir) {
-		// TODO Auto-generated constructor stub
+	
+	public enum Direction{
+		RIGHT,
+		LEFT
 	}
-	int radius;
-	int fps;
+	
+	private int posX;
+	private int posY;
+	private static final int SIZE = 100;
+	
+	private Direction dir;
+	
+	public Bullet(int x, int y, Direction dir) {
+		this.posX = x;
+		this.posY = y;
+		this.dir = dir;
+	}
+	
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+
+	public static int getSize() {
+		return SIZE;
+	}
+
+	public Direction getDir() {
+		return dir;
+	}
 }
