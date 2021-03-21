23fa0dc47272bfaf7e3525d501df47b2f8b7c300
MorlenzaPC <lucamorlino@hotmail.it>
2021-03-21 17:16:45 +0100

    BasicGameApp

---
+++ src/application/BasicGameApp.java
@@ -0,0 +1,53 @@
+package application;
+
+
+
+/**
+ * This class represent the Main class of the JavaFX-based application.
+ */
+
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import java.util.Map;
+
+public class BasicGameApp extends GameApplication {
+    
+	@Override
+	protected void initSettings(GameSettings settings) {
+		settings.setWidth(600);
+		settings.setHeight(600);
+		settings.setTitle("2021 The Last Man Standing");
+		settings.setVersion("0.0");
+	}   
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	private Entity player;
+	
+	@Override
+	protected void initGame() {
+		player = FXGL.entityBuilder()
+				.at(300, 300)
+				.view(new Rectangle(25, 25, Color.BLUE))
+				.buildAndAttach();
+					
+	}
+	
+	@Override
+	protected void initInput() {
+		Input input = FXGL.getInput();
+		
+	}
+}
+
