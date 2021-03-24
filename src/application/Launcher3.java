14e4906ae609299dacaf85327c7bf61a9c4b4930
Luca Cantagallo <luca.cantagallo@studio.unibo.it>
2021-03-24 21:42:48 +0100

    player components

---
+++ src/application/Launcher3.java
@@ -0,0 +1,46 @@
+package application;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.UserAction;
+import javafx.scene.input.KeyCode;
+import model.AnimationComponent;
+
+public class Launcher3 extends GameApplication {
+
+    @Override
+    protected void initSettings(GameSettings settings) { }
+
+    private Entity player;
+
+    @Override
+    protected void initInput() {
+        FXGL.getInput().addAction(new UserAction("Right") {
+            @Override
+            protected void onAction() {
+                player.getComponent(AnimationComponent.class).moveRight();
+            }
+        }, KeyCode.D);
+
+        FXGL.getInput().addAction(new UserAction("Left") {
+            @Override
+            protected void onAction() {
+                player.getComponent(AnimationComponent.class).moveLeft();
+            }
+        }, KeyCode.A);
+    }
+
+    @Override
+    protected void initGame() {
+        player = FXGL.entityBuilder()
+                .at(200, 200)
+                .with(new AnimationComponent())
+                .buildAndAttach();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
