8cc089140d595aa718ad09cdf13d14e7b90e775d
Luca Cantagallo <luca.cantagallo@studio.unibo.it>
2021-03-27 12:43:09 +0100

    player created and basic skills implemented (tested in a provisional world

--- src/model/AnimationComponent.java
+++ src/model/AnimationComponent.java
@@ -3,21 +3,27 @@ package model;
 import com.almasb.fxgl.core.math.FXGLMath;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.component.Component;
+import com.almasb.fxgl.physics.PhysicsComponent;
 import com.almasb.fxgl.texture.AnimatedTexture;
 import com.almasb.fxgl.texture.AnimationChannel;
 import javafx.geometry.Point2D;
 import javafx.util.Duration;
 
 public class AnimationComponent extends Component {
-
-    private int speed = 0;
+		
+	//whithout physics
+    //private int speed = 0;
+    
+    private PhysicsComponent physics;
 
     private AnimatedTexture texture;
     private AnimationChannel animIdle, animWalk;
+    
+    private int jumps = 1;
 
     public AnimationComponent() {
-        animIdle = new AnimationChannel(FXGL.image("JohnTravoltaIdle.png"), 4, 32, 42, Duration.seconds(1), 1, 1);
-        animWalk = new AnimationChannel(FXGL.image("JohnTravoltaRunning.png"), 4, 32, 42, Duration.seconds(1), 0, 7);
+        animIdle = new AnimationChannel(FXGL.image("JTRunning.png"), 8, 112, 134, Duration.seconds(1), 5, 5);
+        animWalk = new AnimationChannel(FXGL.image("JTRunning.png"), 8, 112, 134, Duration.seconds(1), 0, 7);
 
         texture = new AnimatedTexture(animIdle);
     }
@@ -26,36 +32,78 @@ public class AnimationComponent extends Component {
     public void onAdded() {
         entity.getTransformComponent().setScaleOrigin(new Point2D(16, 21));
         entity.getViewComponent().addChild(texture);
+        
+        physics.onGroundProperty().addListener((obs, old, isOnGround) -> {
+            if (isOnGround) {
+
+                jumps = 1; 
+            }
+        });
     }
 
     @Override
-    public void onUpdate(double tpf) {
-        entity.translateX(speed * tpf);
-
-        if (speed != 0) {
-
-            if (texture.getAnimationChannel() == animIdle) {
+    public void onUpdate(double tpf) {   	
+    	if (physics.isMovingX()) {
+            if (texture.getAnimationChannel() != animWalk) {
                 texture.loopAnimationChannel(animWalk);
             }
-
-            speed = (int) (speed * 0.9);
-
-            if (FXGLMath.abs(speed) < 1) {
-                speed = 0;
+        } else {
+            if (texture.getAnimationChannel() != animIdle) {
                 texture.loopAnimationChannel(animIdle);
             }
         }
+    	
+    	//without physics
+//        entity.translateX(speed * tpf);
+//
+//        if (speed != 0) {
+//
+//            if (texture.getAnimationChannel() == animIdle) {
+//                texture.loopAnimationChannel(animWalk);
+//            }
+//
+//            speed = (int) (speed * 0.9);
+//
+//            if (FXGLMath.abs(speed) < 1) {
+//                speed = 0;
+//                texture.loopAnimationChannel(animIdle);
+//            }
+//        }
     }
 
     public void moveRight() {
-        speed = 150;
-
-        getEntity().setScaleX(1);
+    	
+    	getEntity().setScaleX(1); //direzione personaggio
+        physics.setVelocityX(200);
+        
+        //without velocity
+//        speed = 150;
+//
+//        getEntity().setScaleX(1);
     }
 
     public void moveLeft() {
-        speed = -150;
+    	
+    	getEntity().setScaleX(-1); //direzione personaggio
+        physics.setVelocityX(-200);
+        
+        //without physics
+//        speed = -150;
+//
+//        getEntity().setScaleX(-1);
+    }
+    
+    public void stop() {
+        physics.setVelocityX(0);
+    }
+    
+    public void jump() {
+        if (jumps == 0) {
+            return;
+        }
 
-        getEntity().setScaleX(-1);
+        physics.setVelocityY(-450);
+
+        jumps--;
     }
 }
--- src/model/PlayerDirection.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public enum PlayerDirection {
-	LEFT, RIGHT
-}
