630f95a42ac8bdf03214858e491f1aab4dfde206
MorlenzaPC <lucamorlino@hotmail.it>
2021-04-01 23:46:15 +0200

    Set world from input

--- src/application/TheLastManStandingApp.java
+++ src/application/TheLastManStandingApp.java
@@ -10,44 +10,41 @@ package application;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
-import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.level.Level;
+import com.almasb.fxgl.entity.level.tiled.TiledMap;
 import com.almasb.fxgl.input.Input;
-import com.almasb.fxgl.input.UserAction;
-import javafx.scene.input.KeyCode;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
-import java.util.Map;
+
+import factories.WorldFactory;
+import settings.SettingSystemImpl;
+import settings.SettingsSystem;
 
 public class TheLastManStandingApp extends GameApplication {
+	
+	private SettingsSystem mySettings = new SettingSystemImpl();
     
 	@Override
 	protected void initSettings(GameSettings settings) {
-		settings.setWidth(600);
-		settings.setHeight(600);
-		settings.setTitle("2021 The Last Man Standing");
-		settings.setVersion("1.0");
+		settings.setWidth(mySettings.getWidth());
+		settings.setHeight(mySettings.getHeight());
+		settings.setTitle(mySettings.getTitle());
+		settings.setVersion(mySettings.getVersion());
 	}   
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
-	
-	private Entity player;
 	
 	@Override
 	protected void initGame() {
-		player = FXGL.entityBuilder()
-				.at(300, 300)
-				.view(new Rectangle(25, 25, Color.BLUE))
-				.buildAndAttach();
-					
+		FXGL.getGameWorld().addEntityFactory(new WorldFactory());
+		FXGL.setLevelFromMap("Cemetery.tmx");
+	//	FXGL.getGameWorld().spawn("platform", 5.0, 5.0);
 	}
 	
 	@Override
 	protected void initInput() {
 		Input input = FXGL.getInput();
 		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
 
