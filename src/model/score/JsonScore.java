c1dc26f7d5bda63ff1a6d7d6e66e568b8e592ce2
MorlenzaPC <lucamorlino@hotmail.it>
2021-04-24 11:04:49 +0200

    better use of build pattern

--- src/model/score/JsonScore.java
+++ src/model/score/JsonScore.java
@@ -1,23 +1,8 @@
 package model.score;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
-
-
 /**
  * This class implements {@link Score}.
- * It's present a JsonScoreBuilder to build a JsonScore easier and without errors
- * <p>
- * Here is an example of how Builder can be used: 
- * <pre>{@code
- *     JsonScore score = new JsonScore.Builder()
- *                       	.nameFromString("MIKE")
- *                       	.score(150)
- *                       	.build();
- * }</pre>
+ *
  * 
  */
 public final class JsonScore implements Score<String, Integer> {
@@ -79,66 +64,4 @@ public final class JsonScore implements Score<String, Integer> {
 		return name.equals(other.name) 
 		    && score.equals(other.score);	
 	}
-	
-	public static class Builder {
-
-		private String name;
-		private Integer score;
-
-		/**
-		 * @param name
-		 * 	          A string with the user name
-		 * @return
-		 *		  The {@link Builder} builder with user name
-		 */
-		public Builder nameFromString(final String name) {
-			this.name = name;
-			return this;
-		}
-
-		/**
-		 * @param path
-		 * 	          A string with the path of the user name file
-		 * @return The {@link Builder} builder with user name
-		 * @throws FileNotFoundException 
-		 * 		        if the named file does not exist
-		 * @throws IOException
-		 * 		       if an I/O error occurs
-		 */
-		public Builder nameFromPath(final String path) throws FileNotFoundException, IOException {
-			try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
-				this.name = reader.readLine();
-			}
-			return this;
-		}
-
-		/**
-		 * @param score
-		 * 		    The integer score of the game
-		 * @return The {@link Builder} builder with score
-		 */
-		public Builder score(final Integer score) {
-			this.score = score;
-			return this;
-		}
-
-		/**
-		 * 
-		 * @return A {@link Score} only if there are not exceptions
-		 * @throws IllegalStateException
-		 *             if name or score are unset
-		 * @throws IOException
-		 *             if an I/O error occurs
-		 */
-		public Score<String, Integer> build() {
-			if (this.name == null || this.score == null) {
-				System.out.println(name);
-				System.out.println(score);
-				throw new IllegalStateException();
-			}
-			return new JsonScore(name, score);
-		}
-	}
-
-
 }
